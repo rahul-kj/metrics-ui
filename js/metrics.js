3bf928d8d67d3b37caacd26eb15dde267e35561c
Rahul Jain <rahulkj@gmail.com>
2016-01-29 23:40:15 -0600

    making the ui better

--- js/metrics.js
+++ js/metrics.js
@@ -1,5 +1,7 @@
+var endpoint = 'http://metrics.apps.homelab.io';
+var timeOutInterval = 30000;
 var metricsModule = angular
-		.module('metricsApp', [])
+		.module('metricsApp', ["chart.js"])
 		.directive(
 				'loading',
 				function() {
@@ -53,17 +55,58 @@ var metricsModule = angular
 				})
 		.controller(
 				'metricsCtrl',
-				function($scope, $http) {
-					var endpoint = 'http://metrics.apps.homelab.io';
-					$scope.loading = true;
+				function($scope, $timeout, $http) {
 					$scope.dataToLoad = 5;
-					
+
 					$scope.turnOffLoading = function() {
-						if($scope.dataToLoad === 0) { 
+						if($scope.dataToLoad === 0) {
 							$scope.loading = false;
 						}
 					}
 					
+					$scope.populateCharts = function() {
+						$scope.deploymentChartMap = [];
+						uniqueDeploymentNames = [];
+
+						var chartLabels = [];
+						var chartData = [];
+						_.each($scope.vmMetrics, function(vmMetric) {
+							if(!_.contains(uniqueDeploymentNames, vmMetric.jobDetail.deployment)) {
+								uniqueDeploymentNames.push(vmMetric.jobDetail.deployment);
+							}
+						});
+						
+						_.each(uniqueDeploymentNames, function(uniqueDeploymentName) {
+							chartMeta = {};
+							chartMeta["healthydata"] = [];
+							chartMeta["healthylabels"] = [];
+							chartMeta["unhealthydata"] = [];
+							chartMeta["unhealthylabels"] = [];
+							_.each($scope.vmMetrics, function(vmMetric) {
+								if(vmMetric.jobDetail.deployment == uniqueDeploymentName) {
+									if(vmMetric.fixedAttribute.system_healthy == "1.0") {
+										chartMeta.healthylabels.push(vmMetric.jobDetail.job);
+										if(vmMetric.fixedAttribute.system_cpu_user == "0.0") {
+											chartMeta.healthydata.push("0.1");
+										} else {
+											chartMeta.healthydata.push(vmMetric.fixedAttribute.system_cpu_user);
+										}
+									} else {
+										chartMeta.unhealthylabels.push(vmMetric.jobDetail.job);
+										chartMeta.unhealthydata.push(vmMetric.fixedAttribute.system_mem_percent);
+									}
+								}
+							});
+							
+							$scope.deploymentChartMap.push({
+								deploymentname : uniqueDeploymentName,
+								chartMeta : chartMeta
+							});
+						});
+						
+						console.log($scope.deploymentChartMap);
+					}
+
 					$scope.total_app_instances = function() {
 						var total_ai_count = 0;
 						_.each($scope.spaces, function(space) {
@@ -71,10 +114,10 @@ var metricsModule = angular
 								total_ai_count += space.number_of_app_instances;
 							}
 						});
-						
+
 						$scope.total_ai_count = total_ai_count;
 					}
-					
+
 					$scope.app_instances_per_space = function() {
 						_.each($scope.spaces, function(space) {
 							var i = 0;
@@ -89,7 +132,7 @@ var metricsModule = angular
 							space.number_of_app_instances = j;
 						});
 					}
-					
+
 					$scope.totals = function() {
 						_.each($scope.customJobMetrics, function(customJobMetric) {
 							if(customJobMetric.jobDetail.job === "CloudController" && customJobMetric.jobDetail.deployment === "untitled_dev") {
@@ -97,22 +140,23 @@ var metricsModule = angular
 									if(attribute.name === "total_users") {
 										return attribute.value;
 									}
-								}) 
+								})
 							}
-							
+
 							if(customJobMetric.jobDetail.job === "Router" && customJobMetric.jobDetail.deployment === "untitled_dev") {
 								$scope.total_routes = _.find(customJobMetric.customAttributes, function(attribute) {
 									if(attribute.name === "router.total_routes") {
 										return attribute.value;
 									}
-								}) 
+								})
 							}
 						});
 					}
-					
-					$http.get(endpoint + '/totals').success(
+
+					var totals = function() {
+						$http.get(endpoint + '/totals').success(
 							function(data) {
-								$scope.totals = data;
+								$scope.total = data;
 								$scope.dataToLoad--;
 								$scope.turnOffLoading();
 							}).error(
@@ -122,8 +166,10 @@ var metricsModule = angular
 										"Error while invoking the server")
 										.show();
 							});
+					}
 
-					$http
+					var metrics = function() {
+						$http
 							.get(endpoint + '/metrics')
 							.success(
 									function(data) {
@@ -132,6 +178,7 @@ var metricsModule = angular
 										$scope.totals();
 										$scope.dataToLoad--;
 										$scope.turnOffLoading();
+										$scope.populateCharts();
 									})
 							.error(
 									function(response) {
@@ -140,37 +187,45 @@ var metricsModule = angular
 												.html(
 														"Error while invoking the server")
 												.show();
+										console.log(response);
 									});
+					}
 
-					$http.get(endpoint + '/orgs').success(
+					var orgs = function() {
+						$http.get(endpoint + '/orgs').success(
 							function(data) {
 								$scope.orgs = data;
 								$scope.dataToLoad--;
 								$scope.turnOffLoading();
 							}).error(
 							function(response) {
+								console.log(response);
 								$('.alert-danger').html(
 										"Error while invoking the server")
 										.show();
 							});
-					
-					$http.get(endpoint + '/spaces').success(
+					}
+
+					var spaces = function() {
+						$http.get(endpoint + '/spaces').success(
 							function(data) {
 								$scope.spaces = data;
 								$scope.dataToLoad--;
 								$scope.turnOffLoading();
 							}).error(
 							function(response) {
+								console.log(response);
 								$('.alert-danger').html(
 										"Error while invoking the server")
 										.show();
 							});
-					
-					
-					$http.get(endpoint + '/apps').success(
+					}
+
+					var apps = function() {
+						$http.get(endpoint + '/apps').success(
 							function(data) {
 								$scope.apps = data;
-								
+
 								_.each($scope.apps, function(app) {
 									_.find($scope.spaces, function(space) {
 										if(app.space.meta.guid === space.meta.guid) {
@@ -179,13 +234,12 @@ var metricsModule = angular
 										}
 									})
 								});
-								
+
 								$scope.app_instances_per_space();
 								$scope.total_app_instances();
-								
+
 								$scope.dataToLoad--;
 								$scope.turnOffLoading();
-								
 							}).error(
 							function(response) {
 								console.log(response);
@@ -193,5 +247,22 @@ var metricsModule = angular
 										"Error while invoking the server")
 										.show();
 							});
+					}
 					
+					var execute = function() {
+						$scope.loading = true;
+						$('.alert-danger').hide();
+						totals();
+						metrics();
+						orgs();
+						spaces();
+//						apps();
+					}
+					
+					execute();
+//					$timeout(execute, timeOutInterval);
+					
+					
+
 				});
+				
